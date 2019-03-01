299cd0775a94a04043523cd7df0145003441f794
JecaTatu <gas5@cin.ufpe.br>
2019-03-01 18:29:32 -0300

    Novo projeto em node

--- Django/challenge/settings/base.py
+++ Django/challenge/settings/base.py
@@ -16,6 +16,8 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+def base_dir_join(*args):
+    return os.path.join(BASE_DIR, *args)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -119,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = base_dir_join('staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    base_dir_join('assets'),
+)
