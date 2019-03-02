1d55934f78fe0631ac021b87102d59f60432d433
JecaTatu <gas5@cin.ufpe.br>
2019-03-01 21:27:16 -0300

    Criação do canvas do template

--- Node/views/home/home.js
+++ Node/views/home/home.js
@@ -1,1 +1,29 @@
-console.log('It works!');
+//Uploading and showing the template
+var templateUpload = document.getElementById('templateUpload');
+var canvas  = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = new Image();
+           img.src = e.target.result;
+           img.onload = function() {
+             ctx.drawImage(img, 0, 0, 512, 512);
+           };
+        };       
+        FR.readAsDataURL( this.files[0] );
+    }
+}
+
+templateUpload.onchange = readImage;
+
+canvas.onclick = function(e) {
+  var x = e.offsetX;
+  var y = e.offsetY;
+  ctx.beginPath();
+  ctx.fillStyle = 'black';
+  ctx.arc(x, y, 5, 0, Math.PI * 2);
+  ctx.fill();
+};
